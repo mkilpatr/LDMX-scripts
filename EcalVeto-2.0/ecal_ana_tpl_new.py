d27bd0b5f5ade7076a65f93c442d3d18d6fe711a
Matthew Kilpatrick <matthew.kilpatrick@cern.ch>
2020-09-10 15:50:45 -0700

    confirm config for ecal veto

---
+++ EcalVeto-2.0/ecal_ana_tpl_new.py
@@ -0,0 +1,12 @@
+from LDMX.Framework import ldmxcfg
+# Create a process with pass name "vetoana"
+p=ldmxcfg.Process('vetoana')
+from LDMX.Analysis import ecal
+#ecal_veto_ana = ecal.ECalVetoAnalyzer()
+ecal_veto_ana = ecal.ECalMultiElecAnalyzer()
+#you could change the settings if you want/need to
+#ecal_veto_ana.ecal_veto_collection = 'SomeOtherEcalVetoCollection'
+p.sequence = [ ecal_veto_ana ]
+p.inputFiles = ["$inputEventFile" ]
+p.histogramFile = "$histogramFile"
+#p.pause()
